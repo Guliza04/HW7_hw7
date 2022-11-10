057e63cee50cdaebe7de8b748d8aa6c23754804b
unknown <erkubekt@gmail.com>
2022-11-11 01:23:16 +0600

    install botstart

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import './App.css';
+import AboutPage from "./pages/aboutPage/AboutPage";
+import SecondPage from "./pages/secondPage/SecondPage";
+
+function App() {
+  return (
+    <div className="App">
+    <AboutPage/>
+    <SecondPage/>
+    </div>
+  );
+}
+
+export default App;
